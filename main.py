084920ae256438bdd7c02f4b1eb12a2d215cba67
Andrew Gravino <89551299+AndrewG76@users.noreply.github.com>
2022-07-27 06:01:27 +0000

    Changed to respond to "who asked" regardless of case

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ client = discord.Client()
 theOriginalMeme = ["who asked"]
 theTwistedOne = ["i asked"]
 
-@client.event #these 'events' are to listen for stuff that happens
-async def on_ready(): #this is for when the bot successfully logs on the first time
-  print('We have logged in as {0.user}'.format(client))
+
+@client.event  #these 'events' are to listen for stuff that happens
+async def on_ready(
+):  #this is for when the bot successfully logs on the first time
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
-async def on_message(message): #this triggers when a message shows that ISNT the bot itself
-  if message.author == client.user:
-    return #essentially do nothing
+async def on_message(message):  #this triggers when a message shows that ISNT the bot itself
+    if message.author == client.user:
+        return  #essentially do nothing
 
-  if message.content.startswith('?help'):
-    await message.channel.send("say 'who asked' in your message")
+    msg = message.content
 
-  if any(word in msg for word)
+    if msg.startswith('?help'):
+        await message.channel.send("say 'who asked' in your message")
+
+    if any(word in msg.lower() for word in theOriginalMeme):
+        await message.channel.send("i did")
+
+    if any(word in msg.lower() for word in theTwistedOne):
+        await message.channel.send("you too? :flushed:")
+
 
 client.run(os.getenv('TOKEN'))
